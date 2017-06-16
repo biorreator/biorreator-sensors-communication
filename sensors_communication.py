a77a02dd1406aeeaa4628c3de378ed1a5bf32169
Marco Silva <marcowilliamdf@gmail.com>
2017-06-16 16:53:34 -0300

    Create class for ph sensor

--- sensors_communication.py
+++ sensors_communication.py
@@ -3,8 +3,10 @@ from sensors_communication.measure import Measure
 import math
 
 temperature_sensor = TemperatureSensor()
+ph_sensor = PhSensor()
 measure = Measure()
 
 while True:
     measure.temperature = temperature_sensor.collect_data()
+    measure.density = ph_sensor.collect_data()
     measure.push_callback()
--- sensors_communication/sensors.py
+++ sensors_communication/sensors.py
@@ -2,9 +2,10 @@ import math
 import Adafruit_ADS1x15
 
 adc = Adafruit_ADS1x15.ADS1115()
-GAIN = 1
+GAIN = 2/3
 
 class TemperatureSensor():
+
     def collect_data(self):
         temperature_samples = 5
         ds18b20_sum = 0.0
@@ -19,3 +20,15 @@ class TemperatureSensor():
             ds18b20_sum += ds18b20_temperature
         ds18b20_average = ds18b20_sum/temperature_samples
         return ds18b20_average
+
+class PhSensor():
+
+    def collect_data():
+        ph_samples = 20
+        ph_adc_sum = 0.0
+        for i in range(0, ph_samples):
+            print "AQUII"
+            ph_adc = adc.read_adc(0, gain=GAIN)
+            ph_adc_sum += ph_adc
+        ph_adc_average = ph_adc_sum/ph_samples
+        return ph_adc_average
