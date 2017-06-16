a12c252d42e72bf602ccfcc241711ebf7d45c88b
Marco Silva <marcowilliamdf@gmail.com>
2017-06-16 16:23:05 -0300

    Change way of getting teperature on sesor

--- sensors_communication/sensors.py
+++ sensors_communication/sensors.py
@@ -5,14 +5,17 @@ adc = Adafruit_ADS1x15.ADS1115()
 GAIN = 1
 
 class TemperatureSensor():
-    def steinhart_hart(self, temp):
-        return (float(temp)*5.0/(65535))/0.01
-
     def collect_data(self):
         temperature_samples = 100
-        lm35_adc_sum = 0.0
+        ds18b20_sum = 0.0
         for i in range(0, temperature_samples):
-            lm35_adc = adc.read_adc(0, gain=GAIN)
-            lm35_adc_sum += lm35_adc
-        lm35_adc_avg = lm35_adc_sum/temperature_samples
-        return self.steinhart_hart(lm35_adc_avg)
+            tfile = open("/sys/bus/w1/devices/28-041661cf4eff/w1_slave")
+            text = tfile.read()
+            tfile.close()
+            secondline = text.split("\n")[1]
+            ds18b20_data = secondline.split(" ")[9]
+            ds18b20_temperature = float(ds18b20_data[2:])
+            ds18b20_temperature = ds18b20_temperature/1000
+            ds18b20_sum += ds18b20_temperature
+            ds18b20_average = ds18b20_sum/temperature_samples
+        return ds18b20_average
