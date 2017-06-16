0146ac365e18a7657b5561c4e17a42ac6ab8b10d
Marco Silva <marcowilliamdf@gmail.com>
2017-06-16 15:41:48 -0300

    Fix json problems on api communication

--- sensors_communication/measure.py
+++ sensors_communication/measure.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from sensors_communication.env_config import env
 
 class Measure():
@@ -11,11 +12,12 @@ class Measure():
     def push_callback(self):
         url = env["server_address"]+"/api/reaction/2bd80803-0bb1-4387-a6ed-0e4c9a144341/measures"
         data = {
-            'temperature': self.temperature,
-            'density': self.density,
-            'pressureA': self.pressureA,
-            'pressureB': self.pressureB,
+            "temperature": self.temperature,
+            "density": self.density,
+            "pressureA": self.pressureA,
+            "pressureB": self.pressureB,
         }
+        data = json.dumps(data)
         print data
         print url
         # verify if there is some token auth for api
