b6fb8db2a68ea8a0dd0765be68969ded31c0b448
Marco Silva <marcowilliamdf@gmail.com>
2017-06-16 23:11:44 -0300

    Make ultrasonic sensor return distance each 2 seconds

--- sensors_communication/sensors.py
+++ sensors_communication/sensors.py
@@ -73,14 +73,14 @@ class UltrasonicSensor:
             time.sleep(0.00001)
             GPIO.output(TRIG, False)
 
-        while GPIO.input(ECHO)==0:
-            pulse_start = time.time()
+            while GPIO.input(ECHO)==0:
+                pulse_start = time.time()
 
-        while GPIO.input(ECHO)==1:
-            pulse_end = time.time()
+            while GPIO.input(ECHO)==1:
+                pulse_end = time.time()
 
-        pulse_duration = pulse_end - pulse_start
+            pulse_duration = pulse_end - pulse_start
 
-        distance = pulse_duration * 17150
-        distance = round(distance, 2)
-        return distance
+            distance = pulse_duration * 17150
+            distance = round(distance, 2)
+            return distance
