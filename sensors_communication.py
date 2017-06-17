63b4118e91ce03c440630555acae17b645e0e5a5
Marco Silva <marcowilliamdf@gmail.com>
2017-06-16 23:02:39 -0300

    create ultrasonic sensor

--- sensors_communication.py
+++ sensors_communication.py
@@ -1,12 +1,14 @@
 from sensors_communication.sensors import TemperatureSensor
 from sensors_communication.sensors import PhSensor
 from sensors_communication.sensors import PressureSensor
+from sensors_communication.sensors import UltrasonicSensor
 from sensors_communication.measure import Measure
 import math
 
 temperature_sensor = TemperatureSensor()
 ph_sensor = PhSensor()
 pressure_sensor = PressureSensor()
+ultrasonic_sensor = UltrasonicSensor()
 measure = Measure()
 
 while True:
@@ -14,3 +16,4 @@ while True:
     measure.pressureA = ph_sensor.collect_data() #change pressureA to ph
     measure.density = pressure_sensor.collect_data()
     measure.push_callback()
+    print ultrasonic_sensor.collect_data()
--- sensors_communication/sensors.py
+++ sensors_communication/sensors.py
@@ -43,7 +43,44 @@ class PressureSensor():
             tense = (mp5500dp_adc * 0.1875)/1000
             mp5500dp_adc = (tense-0.189)/0.009
 	    density = mp5500dp_adc/(9.81*0.22)
-            mp5500dp_adc_sum += density 
+            mp5500dp_adc_sum += density
         mp5500dp_adc_average = mp5500dp_adc_sum/pressure_samples
 	print "MEDIA TENSAO:"
         return mp5500dp_adc_average
+
+class UltrasonicSensor:
+
+    def collect_data(self):
+        import RPi.GPIO as GPIO                    #Import GPIO library
+        import time                                #Import time library
+        GPIO.setmode(GPIO.BCM)                     #Set GPIO pin numbering
+
+        TRIG = 23                                  #Associate pin 23 to TRIG
+        ECHO = 24                                  #Associate pin 24 to ECHO
+
+        print "Distance measurement in progress"
+
+        GPIO.setup(TRIG,GPIO.OUT)                  #Set pin as GPIO out
+        GPIO.setup(ECHO,GPIO.IN)                   #Set pin as GPIO in
+
+        while True:
+
+            GPIO.output(TRIG, False)                 #Set TRIG as LOW
+            print "Waitng For Sensor To Settle"
+            time.sleep(2)                            #Delay of 2 seconds
+
+            GPIO.output(TRIG, True)                  #Set TRIG as HIGH
+            time.sleep(0.00001)                      #Delay of 0.00001 seconds
+            GPIO.output(TRIG, False)                 #Set TRIG as LOW
+
+        while GPIO.input(ECHO)==0:               #Check whether the ECHO is LOW
+            pulse_start = time.time()              #Saves the last known time of LOW pulse
+
+        while GPIO.input(ECHO)==1:               #Check whether the ECHO is HIGH
+            pulse_end = time.time()                #Saves the last known time of HIGH pulse
+
+        pulse_duration = pulse_end - pulse_start #Get pulse duration to a variable
+
+        distance = pulse_duration * 17150        #Multiply pulse duration by 17150 to get distance
+        return distance = round(distance, 2)            #Round to two decimal points
+                                                 #display out of range
