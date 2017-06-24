b349f18892fceb2b8afb30eed833f9a31a206365
Marco Silva <marcowilliamdf@gmail.com>
2017-06-24 20:14:03 -0300

    Change preassure sensor calibration

--- sensors_communication/sensors.py
+++ sensors_communication/sensors.py
@@ -45,10 +45,14 @@ class PressureSensor():
         for i in range(0, pressure_samples):
             mp5500dp1_adc = adc.read_adc(0, gain=GAIN)
             mp5500dp2_adc = adc.read_adc(2, gain=GAIN)
-            difference = mp5500dp1_adc - mp5500dp2_adc
-            tense = (difference * 0.1875)/1000
-            mp5500dp_adc = (tense-0.189)/0.009
-	    density = mp5500dp_adc/(9.81*0.25)
+            tense_1 = (mp5500dp1_adc * 0.1875)/1000
+            tense_2 = (mp5500dp2_adc * 0.1875)/1000
+            zero_1 = 0.20
+            zedo_2 = 0.19
+            calibration_1 = tense_1 - zero_1
+            calibration_2 = tense_2 - zero_2
+            difference = (calibration_1 - calibration_2)/0.009
+	    density = difference/(9.81*0.25) * 1000
             mp5500dp_adc_sum += density
         mp5500dp_adc_average = mp5500dp_adc_sum/pressure_samples
         return mp5500dp_adc_average
